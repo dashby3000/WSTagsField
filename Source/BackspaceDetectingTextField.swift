a4ff3ffb506c3d3b974ea746127ec13edc694508
Dennis Ashby <dennis.ashby@gmail.com>
2022-03-10 13:06:08 -0800

    Added "Done" button to the Text Input Accessory

--- Source/BackspaceDetectingTextField.swift
+++ Source/BackspaceDetectingTextField.swift
@@ -17,12 +17,25 @@ open class BackspaceDetectingTextField: UITextField {
 
     open var onDeleteBackwards: (() -> Void)?
 
+    @IBInspectable var doneAccessory: Bool {
+        get {
+            return self.doneAccessory
+        }
+        set (hasDone) {
+            if hasDone{
+                addDoneButtonOnKeyboard()
+            }
+        }
+    }
+    
     init() {
         super.init(frame: CGRect.zero)
+        doneAccessory = true
     }
 
     required public init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+        doneAccessory = true
     }
 
     open override func deleteBackward() {
@@ -30,5 +43,22 @@ open class BackspaceDetectingTextField: UITextField {
         // Call super afterwards. The `text` property will return text prior to the delete.
         super.deleteBackward()
     }
-
+    
+    func addDoneButtonOnKeyboard() {
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        doneToolbar.barStyle = .default
+       
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.doneButtonAction))
+       
+        let items = [flexSpace, done]
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+        
+        self.inputAccessoryView = doneToolbar
+    }
+    
+    @objc func doneButtonAction() {
+        self.resignFirstResponder()
+    }
 }
